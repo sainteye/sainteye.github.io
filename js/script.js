03742a4c6f7626d3da4088905c79dc54826ac341
sainteye <sainteye.x.deatheye@gmail.com>
2016-03-10 20:23:18 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();jQuery('.article-body iframe').remove();
+jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();jQuery('.article-body iframe').remove();jQuery('.article-footer').css('overflow', 'hidden');
