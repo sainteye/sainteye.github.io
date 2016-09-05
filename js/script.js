1db6fa78c1242d8fd453e7188681fd69b7bd5dc3
sainteye <sainteye.x.deatheye@gmail.com>
2016-09-06 01:00:36 +0800

    update script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();jQuery('.article-body .adsbygoogle').remove();
+jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');jQuery('.article-header .adsbygoogle').css({height: '120px', overflow: 'hidden'});
