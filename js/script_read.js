25803b23d54f594c520192009ab03bc5bb503ed7
sainteye <sainteye.x.deatheye@gmail.com>
2016-09-06 12:15:08 +0800

    update script.js

--- js/script_read.js
+++ js/script_read.js
@@ -1,3 +1,1 @@
-$('#rdb-article').css('overflow', 'hidden');$('#rdb-article-template').css('width', '90%');
-$('#rdb-article-content,#rdb-article-title').removeAttr('dir');$("img[src*='.gif']").remove();
-$('.topbar-placeholder').remove();
+$('#rdb-article').css('overflow', 'hidden');$('#rdb-article-template').css('width', '90%');$('#rdb-article-content,#rdb-article-title').removeAttr('dir');$("img[src*='.gif']").remove();$('#header').remove();
