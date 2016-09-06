7068ff5972e4043b7b80ed604fb95b2e99fc1789
sainteye <sainteye.x.deatheye@gmail.com>
2016-09-06 11:41:56 +0800

    add extra script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');jQuery('.article-header .adsbygoogle').css({height: '120px', overflow: 'hidden'});
+jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();
+jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();
+jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');
+jQuery('.article-header .adsbygoogle').css({height: '120px', overflow: 'hidden'});
+
+jQuery('a img').parent().css('pointer-events', 'none');
+jQuery("iframe[src*='maps.google.com']").remove();
+jQuery("iframe[src*='www.google.com/maps']").remove();
+jQuery("iframe[src*='facebook.com']").remove();
+jQuery('#main .relative-article').remove();jQuery('.article-body').css('overflow-x', 'hidden');
+jQuery('#recommend-tools').remove();jQuery('#main .comment-form').remove();
+jQuery('#main .article-comments').remove(); jQuery('#header-bar__share-tools').remove();
+jQuery('#header .bar--reading span').remove();
+
+// Yuki
+jQuery('#sb-site #logo').remove(); jQuery('#wrapper #toggleMenu').remove(); jQuery('#float_fb_plugin').remove();
