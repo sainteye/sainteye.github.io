b58113e15c432f0104b5ab9e58f2f623eb81baae
sainteye <sainteye.x.deatheye@gmail.com>
2016-09-06 11:41:56 +0800

    add extra script

---
+++ js/script_read.js
@@ -0,0 +1,1 @@
+$('#rdb-article').css('overflow', 'hidden');$('#rdb-article-template').css('width', '90%');$('#rdb-article-content,#rdb-article-title').removeAttr('dir');$("img[src*='.gif']").remove();
