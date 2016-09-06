b650a5677730ab9595bc4c0ee98c6060fb185929
sainteye <sainteye.x.deatheye@gmail.com>
2016-09-06 11:56:01 +0800

    update read script

--- js/script_read.js
+++ js/script_read.js
@@ -1,1 +1,2 @@
-$('#rdb-article').css('overflow', 'hidden');$('#rdb-article-template').css('width', '90%');$('#rdb-article-content,#rdb-article-title').removeAttr('dir');$("img[src*='.gif']").remove();
+$('#rdb-article').css('overflow', 'hidden');$('#rdb-article-template').css('width', '90%');
+$('#rdb-article-content,#rdb-article-title').removeAttr('dir');$("img[src*='.gif']").remove();
