c5298e30f8db5e415ebcdc2d012da26640f77b8e
sainteye <sainteye.x.deatheye@gmail.com>
2017-04-01 13:16:52 +0800

    update script

--- js/script.js
+++ js/script.js
@@ -5,6 +5,27 @@ var sssheet = (function() {
 	return style.sheet;
 })();
 
+function setCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 var isPixnet = false;
 if (window.location.href.indexOf('pixnet')!=-1) {
 	isPixnet = true;
@@ -38,6 +59,11 @@ if (isPixnet) {
 	if (window.location.href.indexOf('bluehero.pixnet')!=-1) {
 		jQuery('table,embed').width('100%');
 	}
+
+	if (!getCookie('AprilFool2017Event')) {
+		setCookie('AprilFool2017Event', 1);
+	}
+
 }
 else if (window.location.href.indexOf('yukiblog.tw')!=-1) {
 	$('.adsbygoogle').remove();$('.postad').remove();$("iframe").remove();
