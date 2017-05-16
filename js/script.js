66ea5e5549f9221328eba18287b0097161f840f1
sainteye <sainteye.x.deatheye@gmail.com>
2017-05-17 00:04:00 +0800

    update script

--- js/script.js
+++ js/script.js
@@ -5,56 +5,7 @@ var sssheet = (function() {
 	return style.sheet;
 })();
 
-var isPixnet = false;
-if (window.location.href.indexOf('pixnet')!=-1) {
-	isPixnet = true;
-}
-if (window.location.href.indexOf('fupo.tw')!=-1) {
-	isPixnet = true;
-}
-if (window.location.href.indexOf('paulyear.com')!=-1) {
-	isPixnet = true;
-}
-if (window.location.href.indexOf('nellydyu.tw')!=-1) {
-	isPixnet = true;
-}
-if (window.location.href.indexOf('timmyblog.cc')!=-1) {
-	isPixnet = true;
-}
-if (window.location.href.indexOf('mecocute.com')!=-1) {
-	isPixnet = true;
-}
-
-if (isPixnet) {
-	jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();
-	jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();
-	jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');
-	jQuery('.article-header .adsbygoogle').remove();
-	jQuery('.header-ad').css({height: '120px'});
-
-	jQuery('a img').parent().css('pointer-events', 'none');
-	jQuery("iframe[src*='maps.google.com']").remove();
-	jQuery("iframe[src*='www.google.com/maps']").remove();
-	jQuery("iframe[src*='facebook.com']").remove();
-	jQuery('#main .relative-article').remove();jQuery('.article-body').css('overflow-x', 'hidden');
-	jQuery('#recommend-tools').remove();jQuery('#main .comment-form').remove();
-	jQuery('#main .article-comments').remove(); jQuery('#header-bar__share-tools').remove();
-	jQuery('#header .bar--reading span').remove();
-
-	jQuery('#sb-site #logo').remove(); jQuery('#wrapper #toggleMenu').remove(); jQuery('#float_fb_plugin').remove();
-
-	sssheet.insertRule(".pi-ad-mobile-tag-outer { display: none !important; }", 0);
-	if (Math.random() > 0.2) {
-		sssheet.insertRule(".pop-ad-cover { display: none !important; }", 0);
-		sssheet.insertRule("#scupio_interstitialad { display: none !important; }", 0);
-	}
-
-	if (window.location.href.indexOf('bluehero.pixnet')!=-1) {
-		jQuery('table,embed').width('100%');
-	}
-
-}
-else if (window.location.href.indexOf('yukiblog.tw')!=-1) {
+if (window.location.href.indexOf('yukiblog.tw')!=-1) {
 	$('.adsbygoogle').remove();$('.postad').remove();$("iframe").remove();
 }
 else if (window.location.href.indexOf('wiselyview.cc')!=-1) {
@@ -96,6 +47,36 @@ sssheet.insertRule(".pi-Ad-mb-stick { display: none !important; }", 0);
 // sssheet.insertRule(".adsbygoogle ins { display: none !important; }", 0);
 sssheet.insertRule(".nc_wrapper .floatBottom { display: none !important; }", 0);
 
+
+if (jQuery) {
+	jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();
+	jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();
+	jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');
+	jQuery('.article-header .adsbygoogle').remove();
+	jQuery('.header-ad').css({height: '120px'});
+
+	jQuery('a img').parent().css('pointer-events', 'none');
+	jQuery("iframe[src*='maps.google.com']").remove();
+	jQuery("iframe[src*='www.google.com/maps']").remove();
+	jQuery("iframe[src*='facebook.com']").remove();
+	jQuery('#main .relative-article').remove();jQuery('.article-body').css('overflow-x', 'hidden');
+	jQuery('#recommend-tools').remove();jQuery('#main .comment-form').remove();
+	jQuery('#main .article-comments').remove(); jQuery('#header-bar__share-tools').remove();
+	jQuery('#header .bar--reading span').remove();
+
+	jQuery('#sb-site #logo').remove(); jQuery('#wrapper #toggleMenu').remove(); jQuery('#float_fb_plugin').remove();
+
+	sssheet.insertRule(".pi-ad-mobile-tag-outer { display: none !important; }", 0);
+	if (Math.random() > 0.2) {
+		sssheet.insertRule(".pop-ad-cover { display: none !important; }", 0);
+		sssheet.insertRule("#scupio_interstitialad { display: none !important; }", 0);
+	}
+
+	if (window.location.href.indexOf('bluehero.pixnet')!=-1) {
+		jQuery('table,embed').width('100%');
+	}
+
+}
 
 
 
