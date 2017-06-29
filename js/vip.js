ad31f353688143971be53fce0323a68b1c0cce2c
sainteye <sainteye.x.deatheye@gmail.com>
2017-06-29 10:02:59 +0800

    update script

--- js/vip.js
+++ js/vip.js
@@ -1,1 +1,10 @@
-jQuery('.pop-ad-cover').remove();jQuery('.adsbygoogle').remove();jQuery('iframe').remove();
+var sssheet = (function() {
+	var style = document.createElement("style");
+	style.appendChild(document.createTextNode(""));
+	document.head.appendChild(style);
+	return style.sheet;
+})();
+
+sssheet.insertRule(".adsbygoogle { display: none !important; }", 0);
+sssheet.insertRule("iframe { display: none !important; }", 0);
+sssheet.insertRule(".pop-ad-cover { display: none !important; }", 0);
