1306b7624c1ca5d9650f8fa2cc14af5f8bda7b8c
sainteye <sainteye.x.deatheye@gmail.com>
2017-06-29 10:24:58 +0800

    update vip script

--- js/vip.js
+++ js/vip.js
@@ -10,3 +10,4 @@ sssheet.insertRule("iframe { display: none !important; }", 0);
 sssheet.insertRule(".pop-ad-cover { display: none !important; }", 0);
 sssheet.insertRule(".fbAdLink { display: none !important; }", 0);
 sssheet.insertRule(".header-ad { display: none !important; }", 0);
+sssheet.insertRule("#ad_root, #ad_root_2, #ad_root_3 { display: none !important; }", 0);
