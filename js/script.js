86a58b84d1e56829710c19b478b988c3eb0fe52d
sainteye <sainteye.x.deatheye@gmail.com>
2018-07-18 16:55:47 +0800

    add equalizer

--- js/script.js
+++ js/script.js
@@ -90,9 +90,12 @@ if (Math.random() > 0.2) {
 
 
 if (jQuery) {
-	jQuery('#pixad1101d').css('overflow', 'hidden');jQuery('.sticky-box').remove();
-	jQuery('#main').css('overflow', 'hidden');jQuery('.article-body iframe').remove();
-	jQuery('.article-body .adsbygoogle').remove();jQuery('.article-body').removeClass('show-summary');
+	jQuery('#pixad1101d').css('overflow', 'hidden');
+	jQuery('.sticky-box').remove();
+	jQuery('#main').css('overflow', 'hidden');
+	jQuery('.article-body iframe').remove();
+	jQuery('.article-body .adsbygoogle').remove();
+	jQuery('.article-body').removeClass('show-summary');
 	jQuery('.article-header .adsbygoogle').remove();
 	jQuery('.header-ad').css({height: '120px'});
 
@@ -100,12 +103,17 @@ if (jQuery) {
 	jQuery("iframe[src*='maps.google.com']").remove();
 	jQuery("iframe[src*='www.google.com/maps']").remove();
 	jQuery("iframe[src*='facebook.com']").remove();
-	jQuery('#main .relative-article').remove();jQuery('.article-body').css('overflow-x', 'hidden');
-	jQuery('#recommend-tools').remove();jQuery('#main .comment-form').remove();
-	jQuery('#main .article-comments').remove(); jQuery('#header-bar__share-tools').remove();
+	jQuery('#main .relative-article').remove();
+	jQuery('.article-body').css('overflow-x', 'hidden');
+	jQuery('#recommend-tools').remove();
+	jQuery('#main .comment-form').remove();
+	jQuery('#main .article-comments').remove();
+	jQuery('#header-bar__share-tools').remove();
 	jQuery('#header .bar--reading span').remove();
 
-	jQuery('#sb-site #logo').remove(); jQuery('#wrapper #toggleMenu').remove(); jQuery('#float_fb_plugin').remove();
+	jQuery('#sb-site #logo').remove();
+	jQuery('#wrapper #toggleMenu').remove();
+	jQuery('#float_fb_plugin').remove();
 
 	if (window.location.href.indexOf('bluehero.pixnet')!=-1) {
 		jQuery('table,embed').width('100%');
