ff7d5b03513156c202c36fc7f7fa759818414c58
Sean Cheng <sainteye.x.deatheye@gmail.com>
2023-11-26 18:49:07 +0800

    update script

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ if (window.location.href.indexOf('ifoodie.tw') == -1) {
 
   // Remove floating ad
   sssheet.insertRule('#ad-full-page { display: none !important; }', 0)
+  sssheet.insertRule('.Zi_ad_a_H { display: none !important; }', 0)
   sssheet.insertRule(
     'html.show-safari-bottom_menu body { position: relative !important; }',
     0
